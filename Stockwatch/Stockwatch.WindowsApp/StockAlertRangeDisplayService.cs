14d2662ac189d628590fae60f49b34c00c7bc1fa
Adithya Varma <bahadithya@gmail.com>
2024-02-15 08:57:25 +0530

    1. Removed StockPriceServices 2. Added GetComments in StockAlertRangeService

--- Stockwatch/Stockwatch.WindowsApp/StockAlertRangeDisplayService.cs
+++ Stockwatch/Stockwatch.WindowsApp/StockAlertRangeDisplayService.cs
@@ -1,4 +1,5 @@
-﻿using Stockwatch.Model;
+﻿using Stockwatch.Business;
+using Stockwatch.Model;
 using Stockwatch.Model.Dto;
 
 namespace Stockwatch.WindowsApp
@@ -17,16 +18,32 @@ namespace Stockwatch.WindowsApp
             _stockPriceUpdates = stockPriceUpdates;
         }
 
+        public string GetComments(IntraStockPrice currentPrice, StockAlertRange stockAlertRange)
+        {
+            if (currentPrice?.GlobalQuote.Price >= stockAlertRange?.UpperLimit)
+            {
+                return "Stock Price Surge! " + currentPrice.GlobalQuote.Symbol + " stock price value has surged above the Upper Limit";
+            }
+            else if (currentPrice?.GlobalQuote.Price <= stockAlertRange?.LowerLimit)
+            {
+                return "Stock Price Fall! " + currentPrice.GlobalQuote.Symbol + " stock price value has fallen below the Lower Limit";
+            }
+            else
+            {
+                return "Stock in range";
+            }
+        }
+
         public async Task<StockAlertRangeDisplay> GetStockAlertRangeAsync(StockAlertRange stockAlertRange)
         {
-            var stockCurrentPrice = new IntraStockPrice { GlobalQuote = new GlobalQuote() { Price = 50 } }; //await _stockPriceUpdates.GetCurrentPriceAsync(stockAlertRange.StockSymbol);
+            var stockCurrentPrice = await _stockPriceUpdates.GetStockPriceAsync(stockAlertRange.StockSymbol); //new IntraStockPrice { GlobalQuote = new GlobalQuote() { Price = 50 } };
             var stockAlertRangeDisplay = new StockAlertRangeDisplay();
             stockAlertRangeDisplay.StockAlertRangeId = stockAlertRange.Id;
             stockAlertRangeDisplay.UpperLimit = stockAlertRange.UpperLimit;
             stockAlertRangeDisplay.LowerLimit = stockAlertRange.LowerLimit;
             stockAlertRangeDisplay.SymbolId = stockAlertRange.StockSymbol.Id;
             stockAlertRangeDisplay.CurrentPrice = stockCurrentPrice.GlobalQuote.Price;
-            stockAlertRangeDisplay.Comments = _stockPriceUpdates.GetComments(stockCurrentPrice, stockAlertRange);
+            stockAlertRangeDisplay.Comments = GetComments(stockCurrentPrice, stockAlertRange);
             
             return stockAlertRangeDisplay;
         }
